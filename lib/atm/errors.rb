40101d4c2b1d8ecb9bc72dab9d5057fe2564b6fd
Miłosz Sędziak <m.sedziak01@gmail.com>
2017-01-17 18:52:53 +0100

    Finish Errors message

--- lib/atm/errors.rb
+++ lib/atm/errors.rb
@@ -2,6 +2,21 @@
 module TheGlobalBankingSystem
   # will use when something goes wrong
   class Errors
-    # code
+    def type_error
+      puts "Access denied."
+    end
+
+    def pin_is_invalid
+      puts "Access denied: incorrect PIN."
+    end
+
+    def no_money_account
+      puts "You don't have enough money in your account."
+    end
+
+    def no_money_bank
+      puts "We must apologise you because we don't have enough money in this ATM."
+      puts "You need to find another ATM or visit our bank."
+    end
   end
 end
